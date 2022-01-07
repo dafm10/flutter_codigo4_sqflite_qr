a435aa79443bb422944a0cdeae88f20ecca0ddeb
dafm10 <dafm.10@gmail.com>
2022-01-06 20:44:30 -0500

    Form Key

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_codigo4_sqflite_qr/ui/general/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,6 +11,10 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+
+  // para identificar de forma global el formulario usado dentro del padding
+  final _keyForm = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,91 +47,103 @@ class _RegisterPageState extends State<RegisterPage> {
           SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Nombre Completo:",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w700,
-                      color: COLOR_FONT_PRIMARY,
+              child: Form(
+                key: _keyForm,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Nombre Completo:",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w700,
+                        color: COLOR_FONT_PRIMARY,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12.withOpacity(0.05),
-                          blurRadius: 12.0,
-                          offset: Offset(4,4),
-                        ),
-                      ],
+                    const SizedBox(
+                      height: 10.0,
                     ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Nombre Completo",
-                          hintStyle: TextStyle(
-                            color: COLOR_FONT_PRIMARY.withOpacity(0.3),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12.withOpacity(0.05),
+                            blurRadius: 12.0,
+                            offset: Offset(4,4),
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                            borderSide: BorderSide.none,
+                        ],
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Nombre Completo",
+                            hintStyle: TextStyle(
+                              color: COLOR_FONT_PRIMARY.withOpacity(0.3),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                              borderSide: BorderSide.none,
+                            )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    const Text(
+                      "Número de DNI:",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w700,
+                        color: COLOR_FONT_PRIMARY,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12.withOpacity(0.05),
+                            blurRadius: 12.0,
+                            offset: Offset(4,4),
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                            borderSide: BorderSide.none,
-                          )),
+                        ],
+                      ),
+                      child: TextField(
+                        // para habilitar solo teclado de numeros
+                        keyboardType: TextInputType.number,
+                        maxLength: 8, // el maximo de caracteres
+                        // hacemos que solo se ingresen números
+                        // usando expresiones regulares
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                        ],
+                        decoration: InputDecoration(
+                            hintText: "Número de DNI",
+                            hintStyle: TextStyle(
+                              color: COLOR_FONT_PRIMARY.withOpacity(0.3),
+                            ),
+                            counterText: "", // para quitar el contador de caracteres
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                              borderSide: BorderSide.none,
+                            )),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  const Text(
-                    "Número de DNI:",
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w700,
-                      color: COLOR_FONT_PRIMARY,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12.withOpacity(0.05),
-                          blurRadius: 12.0,
-                          offset: Offset(4,4),
-                        ),
-                      ],
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Número de DNI",
-                          hintStyle: TextStyle(
-                            color: COLOR_FONT_PRIMARY.withOpacity(0.3),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                            borderSide: BorderSide.none,
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                            borderSide: BorderSide.none,
-                          )),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
